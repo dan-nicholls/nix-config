01d4fa0a927b332fa44f39007a4d3790a7c31f55
Dan Nicholls <dannicholls12@gmail.com>
2025-04-07 20:09:50 +1000

    feat: add initial dev setup

---
+++ dev.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  programs.tmux = {
+    enable = true;
+  };
+
+  programs.neovim = {
+    enable = true;
+    plugins = with pkgs.vimPlugins; [
+      gruvbox
+    ];
+    extraConfig = ''
+      set number
+      colorscheme gruvbox
+    '';
+  };
+
+  home.packages = with pkgs; [
+    git
+    curl
+    tree
+  ];
+}
