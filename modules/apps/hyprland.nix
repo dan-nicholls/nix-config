810e14447761774437ddad8e760a49ef723dc2c0
Dan Nicholls <dannicholls12@gmail.com>
2025-11-14 14:01:45 +1100

    chore: add additional keybindings for hyprland

--- modules/apps/hyprland.nix
+++ modules/apps/hyprland.nix
@@ -42,7 +42,6 @@
       input = {
         kb_layout = "us";
         kb_options = ["caps:escape"];
-        follow_mouse = 1;
       };
       bind = [
         "$mod, Return, exec, ${config.home.homeDirectory}/.nix-profile/bin/ghostty"
@@ -52,6 +51,49 @@
         "$mod, T, togglefloating"
         "$mod, P, pseudo"
         "$mod, L, exec, hyprlock"
+
+        # Change Focus
+        "$mod, H, movefocus, l"
+        "$mod, L, movefocus, r"
+        "$mod, K, movefocus, u"
+        "$mod, J, movefocus, d"
+
+        # Move Window
+        "$mod SHIFT, H, movewindow, l"
+        "$mod SHIFT, L, movewindow, r"
+        "$mod SHIFT, K, movewindow, u"
+        "$mod SHIFT, J, movewindow, d"
+
+        # Resize Window
+        "$mod CTRL, H, resizeactive, -50 0"
+        "$mod CTRL, L, resizeactive, 50 0"
+        "$mod CTRL, K, resizeactive, 0 -50"
+        "$mod CTRL, J, resizeactive, 0 50"
+
+        # Workspaces
+        "$mod, 1, workspace, 1"
+        "$mod, 2, workspace, 2"
+        "$mod, 3, workspace, 3"
+        "$mod, 4, workspace, 4"
+        "$mod, 5, workspace, 5"
+        "$mod, 6, workspace, 6"
+        "$mod, 7, workspace, 7"
+        "$mod, 8, workspace, 8"
+        "$mod, 9, workspace, 9"
+
+        # Workspaces
+        "$mod SHIFT, 1, movetoworkspace, 1"
+        "$mod SHIFT, 2, movetoworkspace, 2"
+        "$mod SHIFT, 3, movetoworkspace, 3"
+        "$mod SHIFT, 4, movetoworkspace, 4"
+        "$mod SHIFT, 5, movetoworkspace, 5"
+        "$mod SHIFT, 6, movetoworkspace, 6"
+        "$mod SHIFT, 7, movetoworkspace, 7"
+        "$mod SHIFT, 8, movetoworkspace, 8"
+        "$mod SHIFT, 9, movetoworkspace, 9"
+
+        # Toggle split orientation
+        "$mod, V, splitratio, -1"
 
         "$mod, R, exec, ${config.home.homeDirectory}/.nix-profile/bin/rofi -show run"
         "$mod, D, exec, ${config.home.homeDirectory}/.nix-profile/bin/rofi -show drun"
