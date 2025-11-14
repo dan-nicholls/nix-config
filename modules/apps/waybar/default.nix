3f92999e27d17c7e59731a3bd1ddb2e545a3d8b7
Dan Nicholls <dannicholls12@gmail.com>
2025-11-14 18:39:21 +1100

    feat: add waybar configuration

--- modules/apps/waybar/default.nix
+++ modules/apps/waybar/default.nix
@@ -3,22 +3,13 @@
   pkgs,
   lib,
   ...
-}: {
+}: let
+  waybarSettings = builtins.fromJSON (builtins.readFile ./settings.json);
+in {
   programs.waybar = {
     enable = true;
     systemd.enable = true;
     style = ./styles.css;
-    settings = {
-      mainBar = {
-        reload-style-on-change = true;
-        layer = "top";
-        position = "top";
-        spacing = 0;
-        height = 26;
-        modules-left = ["hyprland/workspaces"];
-        modules-center = ["clock"];
-        modules-right = ["group/tray-expander" "bluetooth" "network" "pulseaudio" "cpu" "battery"];
-      };
-    };
+    settings = waybarSettings;
   };
 }
