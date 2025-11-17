e020386c2a2624571481802d89068fe9359f6bb7
Dan Nicholls <dannicholls12@gmail.com>
2025-11-18 09:55:27 +1100

    chore: add styling for rofi

--- modules/apps/rofi/default.nix
+++ modules/apps/rofi/default.nix
@@ -8,5 +8,21 @@
     enable = true;
 
     theme = ./theme.rasi;
+
+    extraConfig = {
+      show-icons = true;
+      icon-theme = "Papirus-Dark";
+      modi = "drun,run,filebrowser,window";
+      cycle = true;
+      scroll-method = 0;
+      normalize-match = true;
+      case-sensitive = false;
+
+      display-drun = "Apps";
+      click-to-exit = true;
+
+      kb-mode-next = "Shift+Right,Control+Tab";
+      kb-mode-previous = "Shift+Left";
+    };
   };
 }
