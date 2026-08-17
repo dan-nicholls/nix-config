2fb811c6c6646c0d640a0890e88521952463d6d0
Dan Nicholls <dannicholls12@gmail.com>
2026-08-17 20:03:20 +1000

    refactor: move hosts to modules folder

--- flake.nix
+++ flake.nix
@@ -2,21 +2,25 @@
   description = "nixOS configuration";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-     nvf = {
-     	url = "github:NotAShelf/nvf";
-	inputs.nixpkgs.follows = "nixpkgs";
-     };
+    nvf = {
+      url = "github:NotAShelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, nvf, ... }: {
+  outputs = {
+    nixpkgs,
+    nvf,
+    ...
+  }: {
     nixosConfigurations.x1-carbon = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         nvf.nixosModules.default
-        ./hosts/x1-carbon/configuration.nix
-	./modules/apps/tmux.nix
-	./modules/common/caps-swap-tty.nix
-	./modules/apps/nvf.nix
+        ./modules/hosts/x1-carbon/configuration.nix
+        ./modules/apps/tmux.nix
+        ./modules/common/caps-swap-tty.nix
+        ./modules/apps/nvf.nix
       ];
     };
   };
