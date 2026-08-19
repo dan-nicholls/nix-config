5cf2a5f5556b9869d214445211beda9be58afc72
Dan Nicholls <dannicholls12@gmail.com>
2026-08-19 10:34:14 +1000

    feat: add niri module

---
+++ modules/desktop/niri.nix
@@ -0,0 +1,44 @@
+{
+  self,
+  inputs,
+  ...
+}: {
+  flake.nixosModules.niri = {
+    pkgs,
+    lib,
+    ...
+  }: {
+    programs.niri = {
+      enable = true;
+      package = self.packages.${pkgs.stdenv.hostPlatform.system}.myNiri;
+    };
+  };
+
+  perSystem = {
+    pkgs,
+    lib,
+    self',
+    ...
+  }: {
+    packages.myNiri = inputs.wrapper-modules.wrappers.niri.wrap {
+      inherit pkgs;
+
+      settings = {
+        spawn-at-startup = [
+          (lib.getExe self'.packages.myNoctalia)
+        ];
+        xwayland-satellite.path = lib.getExe pkgs.xwayland-satellite;
+
+        input.keyboard.xkb.layout = "us,ua";
+
+        layout.gaps = 5;
+
+        binds = {
+          "Mod+Return".spawn-sh = lib.getExe pkgs.ghostty;
+          "Mod+Q".close-window = _: {};
+          "Mod+R".spawn-sh = "${lib.getExe self'.packages.myNoctalia} ipc call launcher toggle";
+        };
+      };
+    };
+  };
+}
