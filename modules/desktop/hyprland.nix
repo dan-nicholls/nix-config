a87b7238113f1d35f524dc974e701d7d8be3021e
Dan Nicholls <dannicholls12@gmail.com>
2026-08-19 10:35:14 +1000

    feat: move hyprland to module

--- modules/desktop/hyprland.nix
+++ modules/desktop/hyprland.nix
@@ -1,37 +1,60 @@
-{pkgs, ...}: let
-  wallpaper = ../../assets/backgrounds/2-forest.jpg;
-in {
-  programs.hyprland = {
-    enable = true;
-  };
-
-  services.logind.settings.Login = {
-    HandleLidSwitch = "ignore";
-    HandleLidSwitchDocked = "ignore";
-  };
-
-  environment.systemPackages = [pkgs.hyprpaper];
-
-  services.greetd = {
-    enable = true;
-
-    settings.default_session = {
-      command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd 'start-hyprland -- --config /etc/xdg/hypr/hyprland.lua'";
-      user = "greeter";
+{
+  self,
+  inputs,
+  ...
+}: {
+  flake.nixosModules.hyprland = {pkgs, ...}: let
+    wallpaper = ../../assets/backgrounds/2-forest.jpg;
+  in {
+    programs.hyprland = {
+      enable = true;
+      package = self.packages.${pkgs.stdenv.hostPlatform.system}.myHyprland;
     };
+
+    services.logind.settings.Login = {
+      HandleLidSwitch = "ignore";
+      HandleLidSwitchDocked = "ignore";
+    };
+
+    environment.systemPackages = [pkgs.hyprpaper];
+
+    services.greetd = {
+      enable = true;
+
+      settings.default_session = {
+        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd 'start-hyprland -- --config /etc/xdg/hypr/hyprland.lua'";
+        user = "greeter";
+      };
+    };
+
+    environment.sessionVariables = {
+      NIXOS_OZONE_WL = "1";
+    };
+
+    environment.etc."xdg/hypr/hyprland.lua".source = ./hyprland.lua;
+    environment.etc."xdg/hypr/hyprpaper.conf".text = ''
+      splash = false
+
+      wallpaper {
+        monitor = *
+        path = ${wallpaper}
+      }
+    '';
   };
 
-  environment.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
+  perSystem = {
+    pkgs,
+    lib,
+    self',
+    ...
+  }: {
+    packages.myHyprland = inputs.wrapper-modules.lib.wrapPackage ({...}: {
+      inherit pkgs;
+
+      package = pkgs.hyprland;
+      flags."--config" = "${./hyprland.lua}";
+
+      passthru.providedSessions = pkgs.hyprland.passthru.providedSessions;
+    });
   };
-
-  environment.etc."xdg/hypr/hyprland.lua".source = ./hyprland.lua;
-  environment.etc."xdg/hypr/hyprpaper.conf".text = ''
-    splash = false
-
-    wallpaper {
-      monitor = *
-      path = ${wallpaper}
-    }
-  '';
 }
